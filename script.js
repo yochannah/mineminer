fdd30f622771eda18e316ffcfd45154f6cac45df
Yochannah Yehudi <yochannah@gmail.com>
2016-01-30 12:27:22 +0000

    added dummy mine list

---
+++ script.js
@@ -0,0 +1,34 @@
+var mines = {
+  flymine: {
+    name: "FlyMine",
+    location: {
+      lat : 52.2003296,
+      lng : 0.1198779
+    },
+    url : "http://www.flymine.org"
+  },
+  humanmine: {
+      name: "HumanMine",
+      location: {
+        lat : 52.2003296,
+        lng : 0.1198779
+      },
+      url : "http://www.humanmine.org"
+    },
+  targetmine : {
+    name: "TargetMine",
+    location: {
+      lat:34.8528352,
+      lon:135.5192669
+    },
+    url: "http://targetmine.mizuguchilab.org/"
+  },
+  mousemine : {
+    name: "MouseMine",
+    location: {
+      lat:44.3662447,
+      lon:-68.1987745
+    },
+    url : "http://www.mousemine.org/mousemine/begin.do"
+  }
+};
